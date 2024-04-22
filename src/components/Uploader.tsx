b22feb5e2a3da571a2fc76f58bad84584ca130cb
alison <71641175+damacusdos@users.noreply.github.com>
2024-04-22 23:59:44 +0800

    add upload component hover style

--- src/components/Uploader.tsx
+++ src/components/Uploader.tsx
@@ -5,10 +5,10 @@ export const Uploader = () => {
   const fileInputRef = React.useRef<HTMLInputElement>(null);
 
   return (
-    <div className="relative z-[1] w-[440px] h-40 rounded-lg border border-transparent before:content-[''] before:w-[440px] before:absolute before:top-[-1px] before:bottom-[-1px] before:right-[-1px] brefore:left-[-1px] before:z-[-1] before:bg-[repeating-linear-gradient(135deg,_transparent_0_4px,_#F3F0FF_4px_8px)] before:bg-[#C1B2FA] before:rounded-lg">
+    <div className="relative z-[1] w-[440px] h-40 rounded-lg border border-transparent before:content-[''] before:w-[440px] before:absolute before:top-[-1px] before:bottom-[-1px] before:right-[-1px] brefore:left-[-1px] before:z-[-1] before:bg-[repeating-linear-gradient(135deg,_transparent_0_4px,_#F3F0FF_4px_8px)] before:bg-[#C1B2FA] before:rounded-lg before:hover:bg-[#9a82f7] before:transition-all">
       <button
         onClick={() => fileInputRef.current?.click()}
-        className="w-full h-full flex flex-col justify-center items-center bg-[#F3F0FF] rounded-lg"
+        className="w-full h-full flex flex-col justify-center items-center bg-[#F3F0FF] hover:bg-[#EBE5FF] rounded-lg"
       >
         <CloudArrowUp />
         <h1 className="text-[#7A5FEC] text-[16px] mb-[1px] mt-3">
